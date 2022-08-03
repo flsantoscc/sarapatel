36a255dff326261e64c9667d3d12333d5843cd35
Flavio Emanuel De Oliveira Santos <201900016974@office.ufs.br>
2022-08-03 14:22:35 -0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+/* Create an AST, then invoke our interpreter. */
+import simpleAdder.interpret.Interpreter;
+import simpleAdder.parser.* ;
+import simpleAdder.lexer.* ;
+import simpleAdder.node.* ;
+
+import java.io.* ;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            try {
+                // AST Setup
+                File file = new File("src/" + args[0]).getAbsoluteFile();
+
+                Lexer lexer = new Lexer (new PushbackReader(
+                        new FileReader(file), 1024));
+                Parser parser = new Parser(lexer);
+                Start ast = parser.parse() ;
+
+                // Interpreter Setup
+                Interpreter interp = new Interpreter () ;
+                ast.apply(interp) ;
+            }
+            catch (Exception e) {
+                System.out.println (e) ;
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("usage: java simpleAdder inputFile");
+            System.exit(1);
+        }
+    }
+}
