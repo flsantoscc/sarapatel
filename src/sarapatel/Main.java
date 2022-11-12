62e7d3afe4e543b4a68b1a85eb7c7c2b4b095313
Igor Lisboa <igor.lisboa30@dcomp.ufs.br>
2022-11-11 22:46:53 -0300

    Ponto de entrada do compilador montado.

--- src/sarapatel/Main.java
+++ src/sarapatel/Main.java
@@ -1,7 +1,45 @@
 package sarapatel;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PushbackReader;
+
+import sarapatel.lexer.Lexer;
+import sarapatel.node.Start;
+import sarapatel.parser.Parser;
+import sarapatel.semantic.SemanticAnalyzer;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            PushbackReader pr = new PushbackReader(new BufferedReader(new FileReader(args[0])), 1024);
 
+            Lexer l = new Lexer(pr);
+
+            System.out.println("Parsing...");
+            Parser p = new Parser(l);
+
+            Start tree = p.parse();
+
+            System.out.println("Checking... ");
+            tree.apply(new SemanticAnalyzer());
+
+            // System.out.println("Generating P4-Code");
+            // CodeGenerator gen = new CodeGenerator();
+            // tree.apply(gen);
+
+            pr.close();
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
