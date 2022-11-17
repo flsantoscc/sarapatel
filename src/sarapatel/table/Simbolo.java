489857569d1ce58ca4ebcaed98e97f3afb61b29a
Igor Lisboa <igor.lisboa30@dcomp.ufs.br>
2022-11-17 18:11:34 -0300

    Campo indicado tipo do operador adicionado.

--- src/sarapatel/table/Simbolo.java
+++ src/sarapatel/table/Simbolo.java
@@ -1,15 +1,19 @@
 package sarapatel.table;
 
+import sarapatel.table.Operacao.OPERACAO;
+
 /**
  * Simbolo
  */
 public class Simbolo {
     private String id;
     private String tipo;
+    private OPERACAO op;
 
-    public Simbolo(String id, String tipo) {
+    public Simbolo(String id, String tipo, OPERACAO op) {
         this.id = id;
         this.tipo = tipo;
+        this.op = op;
     }
 
     public String getId() {
@@ -18,6 +22,10 @@ public class Simbolo {
 
     public String getTipo() {
         return tipo;
+    }
+
+    public OPERACAO getOp() {
+        return op;
     }
 
     @Override
